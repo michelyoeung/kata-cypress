5cdee32bb2a402f33ddeae5044ea77cec4ea7187
Michel Yoeung <michel.yoeung@neoxia.com>
2022-10-13 20:15:58 +0200

    setup env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bouffe-roulette';
+
+  ngOnInit(): void {
+    console.log('[TEST] environment.googleApiKey : ', environment.googleApiKey);
+  }
 }
