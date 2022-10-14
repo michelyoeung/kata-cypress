efb0feeeb47a903a4a75a5a3db573efe4471905a
Leopold Bancal <leopold.bancal@neoxia.com>
2022-10-14 13:53:07 +0200

    fix

--- src/app/steps/step-five/step-five.component.ts
+++ src/app/steps/step-five/step-five.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, OnInit, Input, Output} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {catchError, map} from "rxjs/operators";
 import {Observable, of} from "rxjs";
+import {GOOGLE_API_KEY} from "../../../../env";
 
 @Component({
   selector: 'app-step-five',
@@ -14,7 +15,7 @@ export class StepFiveComponent implements OnInit {
   apiLoaded: Observable<boolean>;
 
   constructor(private httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE', 'callback')
+    this.apiLoaded = httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`, 'callback')
       .pipe(
         map(() => true),
         catchError(() => of(false)),
