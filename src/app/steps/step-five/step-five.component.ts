0f37cc6c409d11c621a106886649e87a884d02c0
Michel Yoeung <michel.yoeung@neoxia.com>
2022-10-14 14:47:22 +0200

    add step maps

--- src/app/steps/step-five/step-five.component.ts
+++ src/app/steps/step-five/step-five.component.ts
@@ -1,9 +1,4 @@
 import {Component, EventEmitter, OnInit, Input, Output} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {catchError, map} from "rxjs/operators";
-import {Observable, of} from "rxjs";
-import {GOOGLE_API_KEY} from "../../../../env";
-import {ApiService} from "../../api.service";
 
 @Component({
   selector: 'app-step-five',
@@ -13,18 +8,10 @@ import {ApiService} from "../../api.service";
 export class StepFiveComponent implements OnInit {
   @Input() stepData: any = {};
   @Output() onNextStep = new EventEmitter<string>();
-  apiLoaded: Observable<boolean>;
 
-  constructor(private httpClient: HttpClient, private _api: ApiService) {
-    this.apiLoaded = httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`, 'callback')
-      .pipe(
-        map(() => true),
-        catchError(() => of(false)),
-      );
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    this._api.getLocation();
     console.log(this.stepData);
   }
 
