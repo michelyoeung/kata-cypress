0a03bb251812bb6aa2bbdee2b6bcbb225a1c69c9
Leopold Bancal <leopold.bancal@neoxia.com>
2022-10-14 13:23:08 +0200

    oui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, of, tap} from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { GOOGLE_API_KEY } from "../../env";
 
 @Component({
   selector: 'app-root',
@@ -6,8 +10,40 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  //apiLoaded: Observable<boolean>;
+  choiceOfRestaurant: string[];
 
-  ngOnInit(): void {}
+  constructor(private httpClient: HttpClient) {
+    /*this.apiLoaded = httpClient.jsonp(`https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`, 'callback')
+      .pipe(
+        map(() => true),
+        catchError(() => of(false)),
+      );*/
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition( (position)=>{
+        console.log("position", position)
+        this.getNearbyRestaurant(position)
+      });
+    } else {
+      console.log("No support for geolocation")
+    }
+  }
+  getNearbyRestaurant(position: GeolocationPosition){
+   const restaurant = this.httpClient.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.coords.latitude}%2C${position.coords.longitude}&radius=1500&type=restaurant&key=${GOOGLE_API_KEY}`)
+   restaurant.subscribe((res: any)=> {
+     res.results.map((restaurant: any) => {
+       this.choiceOfRestaurant.push((restaurant.name));
+     })
+     return res
+   })
+  }
+
+  ngOnInit(): void {
+    this.getLocation();
+  }
 
   title = 'bouffe-roulette';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,11 +11,14 @@ import { FoodRouletteComponent } from './food-roulette/food-roulette.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FoodRouletteComponent
+    FoodRouletteComponent,
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        GoogleMapsModule,
+        HttpClientModule,
+        HttpClientJsonpModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
