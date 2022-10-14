40b9f36d4bd3e0376d634bd4fa17396b130dce06
Guerroui Mohammed Naim <mohamednaim.guerroui>
2022-10-14 15:45:11 +0200

    feature(pwa): responsivee + rotate

--- src/app/steps/step-one/step-one.component.ts
+++ src/app/steps/step-one/step-one.component.ts
@@ -7,12 +7,15 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class StepOneComponent implements OnInit {
   @Output() onNextStep = new EventEmitter<string>();
-
+  rotate = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  click(): void {
+    this.rotate = !this.rotate;
+  }
   goNextStep(selected: string): void {
     this.onNextStep.emit(selected);
   }
