1c66b7ab697407801b23439fa8731031dfb49849
Leopold Bancal <leopold.bancal@neoxia.com>
2022-11-15 17:08:24 +0100

    add test and data-cy

--- cypress/e2e/app.spec.cy.ts
+++ cypress/e2e/app.spec.cy.ts
@@ -1,4 +1,16 @@
 describe('empty spec', () => {
+  it('Create profile', () => {
+    cy.visit('http://localhost:4200');
+    cy.get('[data-cy="changeProfile"]').click();
+    cy.get('[data-cy="inputName"]').type("Léopold")
+    cy.get('[data-cy="inputAge"]').type("25")
+    cy.get('[data-cy="inputProfilePic"]').should('exist')
+    cy.get('[data-cy="inputCheck"]').click()
+    cy.get('[data-cy="formValid"]').click()
+    cy.wait(5000);
+    // Add profile pic and Check profile into and toatsr info
+  });
+
   it('passes', () => {
     cy.visit('http://localhost:4200');
     cy.contains('Ça part en prod!!!').click();
